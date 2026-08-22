cbe9724208c9bae08c1d8d679a2fc28c9dcc87f5
Esha Abbasi <eshaashfar@gmail.com>
2026-08-22 14:58:57 +0300

    user can create book from and saved to db

--- controllers/bookCtrl.js
+++ controllers/bookCtrl.js
@@ -10,6 +10,26 @@ const Book = require('../models/book');
     res.redirect('/');
   }
 };
+const newApp =async(req,res)=>{
+  try{ res.render('book/new.ejs')}
+catch (err) {
+    console.log(err);
+    res.redirect('/');
+  }
+}
+const create = async (req, res) => {
+  try {
+    const book = new Book(req.body);
+    book.user = req.session.user._id; // or req.params.id, depending on Option A/B above
+    await book.save();
+    res.redirect('/books');
+  } catch (err) {
+    console.log(err);
+    res.redirect('/books/new');
+  }
+};
 module.exports={
-  index,
+index,
+new:newApp,
+create,
 }
--- routes/BookRouter.js
+++ routes/BookRouter.js
@@ -5,8 +5,10 @@ const router = express.Router({ mergeParams: true });
 
 const bookCtrl = require('../controllers/bookCtrl');
  
- router.get('/', isSignedIn,bookCtrl.index);//get
-
+ router.get('/', isSignedIn,bookCtrl.index);//get-it will show all books
+ router.get('/new',isSignedIn,bookCtrl.new);//add books
+router.post('/',isSignedIn,bookCtrl.create);
+//router.get('/:bookId')
 
 
 
